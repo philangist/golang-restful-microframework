19a75bdb32dac2e9bbab16ca999d4d92b89d4efc
Phil Opaola <phil.opaola@percolate.com>
2014-08-12 01:15:30 -0400

    Add HelloResource http endpoints

---
+++ hello_world_resource.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "net/url"
+  "sleepy"
+  )
+
+type HelloResource struct {
+  sleepy.PostNotSupported
+  sleepy.PutNotSupported
+  sleepy.DeleteNotSupported
+}
+
+func (HelloResource) Get(values url.Values) (int, interface{}) {
+  data := map[string]string{"hello": "world"}
+  return 200, data
+}
+
+func main(){
+  helloResource := new(HelloResource)
+
+  var api = new(sleepy.API)
+  api.addResource(helloResource, "/hello")
+  api.Start(3000)
+}
